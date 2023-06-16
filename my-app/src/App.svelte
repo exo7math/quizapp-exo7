754d7eadb4c64230eb6c66336a02036f72991f1b
Phil1010 <philliptysoun@gmail.com>
2023-06-16 09:23:27 +0200

    Push pour faire la demande de pull request sur le vrai repo

--- my-app/src/App.svelte
+++ my-app/src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Question from './Question.svelte';
   import { onMount } from 'svelte';
+  import jQuery from 'jquery';
 
   // catalogue.json
   let donnees;
@@ -16,21 +17,49 @@
   let corrections = [];
   // indice de la proposition sélectionnée par l'utilisateur pour chaque question
   let selectedValue = null;
-  // score +1 si bonne réponse 0 sinon
-  let score = 0;
+  // score +1 sinon enlever autant de points que le pourcentage de pénalité
+  let scoreSur20 = 0;
+  let noteSur100 = 0;
+  let nbQuestionReussi = 0;
+  let nbQuestionEchoue = 0;
+  let nbQuestionSaute = 0;
 
-  const listeQuestionsDansURL = new URLSearchParams(window.location.search);
-  const listeQuestionsString = listeQuestionsDansURL.get('listeQuestions');
-  const rand = listeQuestionsDansURL.get('rand');
+  // tableau contenant les valeurs sélectionnées par question
+  let tabValeurSelectionner = [];
 
-  function getListeQuestions() {
-    if (listeQuestionsString == null) {
-      document.querySelector('.question').textContent = 'Quiz non disponible.';
-      document.querySelector('#btnNext').style.display = 'none';
-      return;
-    }
-    listeQuestions.push(...listeQuestionsString.split(','));
-    listeQuestions = listeQuestions.map((question) => parseInt(question));
+  const urlDeLaPage = new URLSearchParams(window.location.search);
+  const listeQuestionsString = urlDeLaPage.get('c');
+  const rand = urlDeLaPage.get('r');
+  const penalite = urlDeLaPage.get('p');
+  let idQuizHache = urlDeLaPage.get('i');
+  let nickName = urlDeLaPage.get('n');
+
+  const userID = getUserId();
+  let resultatAEnvoyerAuPHP = [];
+
+  let nbQuestions;
+
+  let timer; // Référence de l'intervalle du timer
+  let temps = 0; // Temps écoulé en secondes
+
+  onMount(() => {
+    fetchData();
+    getListeQuestions();
+  });
+
+  function generateUniqueID() {
+    // Générer un ID unique, par exemple en utilisant un algorithme de hachage ou une bibliothèque externe
+    // Dans cet exemple, nous utilisons simplement un timestamp pour des fins de démonstration
+    return Date.now().toString();
+  }
+
+  function startTimer() {
+    timer = setInterval(() => {
+      temps++;
+    }, 1000);
+  }
+  function stopTimer() {
+    clearInterval(timer); // Arrêter l'intervalle du timer
   }
 
   function handleSelectedValue(value) {
@@ -57,12 +86,8 @@
     } catch (error) {
       console.error('Erreur lors de la récupération des données :', error);
     }
+    if (listeQuestionsString != null) getNbQuestions();
   }
-
-  onMount(() => {
-    fetchData();
-    getListeQuestions();
-  });
 
   function fetchQuestions() {
     if (donnees) {
@@ -81,44 +106,387 @@
         }
       });
 
-      if (rand == 'true') {
+      if (rand == '1') {
         shuffleQuestions();
       }
+    }
+  }
+
+  function demarrer() {
+    document.querySelector('.accueil').style.display = 'none';
+    document.querySelector('#btnNext').style.display = '';
+    document.querySelector('.question').style.display = '';
+    startTimer();
+    if (nickName == null) {
+      nickName = document.querySelector('#pseudo').value;
     }
   }
 
   function nextQuestion() {
     questionIndex++;
     if (selectedValue == corrections[questionIndex - 1].indexOf(true)) {
-      score = score + 1;
+      scoreSur20++;
+      nbQuestionReussi++;
+      resultatAEnvoyerAuPHP.push(1);
+    } else if (selectedValue == null || selectedValue.length > 1) {
+      nbQuestionSaute++;
+      resultatAEnvoyerAuPHP.push(0);
     } else {
-      score = score;
+      const scorePenalite = scoreSur20 - p / 100; // Applique la pénalité en soustrayant le pourcentage p du score sur 20
+      scoreSur20 = scorePenalite;
+      nbQuestionEchoue++;
+      resultatAEnvoyerAuPHP.push(-1);
     }
     if (questionIndex == questions.length) {
+      if (scoreSur20 < 0) {
+        scoreSur20 = 0;
+      }
+      scoreSur20 = (scoreSur20 * 20) / questions.length;
       finQuiz();
     }
+    tabValeurSelectionner.push(propositions[questionIndex - 1][selectedValue]);
+    selectedValue = null;
   }
 
   function finQuiz() {
     document.querySelector('.score').style.display = '';
     document.querySelector('#btnNext').style.display = 'none';
+    document.querySelector('#btnRejouer').style.display = '';
+    document.querySelector('.timer').style.display = '';
+    document.querySelector('.detailsQuestions').style.display = '';
+    document.querySelector('#voirCorrection').style.display = '';
+    stopTimer();
+    getNoteSur100();
+    resultatAEnvoyerAuPHP = resultatAEnvoyerAuPHP.map((resultat) => {
+      if (resultat == -1) {
+        return 2;
+      }
+      if (resultat == 0) {
+        return 0;
+      }
+      if (resultat == 1) {
+        return 1;
+      }
+    });
+    resultatAEnvoyerAuPHP = resultatAEnvoyerAuPHP.join('');
+    envoyerDonneesQuizFini();
+    //envoyerStats();
+  }
+
+  function envoyerDonneesQuizFini() {
+    if (idQuizHache == null) idQuizHache = '';
+    if (nickName == null) nickName = '';
+
+    jQuery.ajax({
+      url: 'https://damienmegy.xyz/php/quizapp-exo7/quiz_fini.php',
+      method: 'POST',
+      data: {
+        quizID: JSON.stringify(idQuizHache),
+        quizContent: JSON.stringify(listeQuestionsString),
+        userID: JSON.stringify(userID),
+        userName: JSON.stringify(nickName),
+        results: JSON.stringify(resultatAEnvoyerAuPHP),
+        elapsedTime: JSON.stringify(temps),
+        note: JSON.stringify(noteSur100.toString()),
+        penalite: JSON.stringify(penalite),
+      },
+
+      success: function (response) {
+        console.log('Réponse du server : ');
+        console.log(response);
+      },
+      error: function (xhr, status, error) {
+        console.error(error);
+      },
+    });
+  }
+
+  function envoyerStats() {
+    var quizID = idQuizHache;
+    var xhr = new XMLHttpRequest();
+    xhr.open(
+      'POST',
+      'http://localhost/quizapp-exo7/backend/quiz_stats.php',
+      true
+    );
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          var responseData = JSON.parse(xhr.responseText);
+          console.log('Réponse du serveur :', responseData);
+          // Traitement des données retournées
+        } else {
+          console.error(
+            'Erreur lors de la requête :',
+            xhr.status,
+            xhr.statusText
+          );
+          // Gestion des erreurs
+        }
+      }
+    };
+
+    var data = 'quizID=' + encodeURIComponent(quizID);
+    xhr.send(data);
+  }
+
+  function rejouer() {
+    if (nickName != '') {
+      const url = new URL(window.location.href);
+      url.searchParams.set('n', nickName); // Ajouter le nouveau pseudo comme paramètre 'n' dans l'URL
+      window.location.href = url.toString(); // Recharger la page avec les nouveaux paramètres
+    } else {
+      window.location.reload();
+    }
+  }
+
+  function cliqueCorrection() {
+    affichageCorrection();
+    document.querySelector('#voirCorrection').style.display = 'none';
+    document.querySelector('#cacherCorrection').style.display = '';
+  }
+
+  function affichageCorrection() {
+    var exerciceContainer = document.getElementById('ligneEnonceCompile');
+    var table = document.createElement('table'); // Création du tableau
+    table.style.borderCollapse = 'collapse'; // Ajout de la propriété de fusion des bordures
+
+    // Création de la première ligne avec les titres
+    var headerRow = table.insertRow();
+    var questionHeaderCell = headerRow.insertCell();
+    questionHeaderCell.innerHTML = 'Questions';
+    questionHeaderCell.style.fontWeight = 'bold';
+    questionHeaderCell.style.border = '1px solid white'; // Ajout de la bordure à la cellule du titre
+
+    var correctionHeaderCell = headerRow.insertCell();
+    correctionHeaderCell.innerHTML = 'Corrections';
+    correctionHeaderCell.style.fontWeight = 'bold';
+    correctionHeaderCell.style.border = '1px solid white'; // Ajout de la bordure à la cellule du titre
+
+    var correctionHeaderCell = headerRow.insertCell();
+    correctionHeaderCell.innerHTML = 'Vos réponses';
+    correctionHeaderCell.style.fontWeight = 'bold';
+    correctionHeaderCell.style.border = '1px solid white';
+
+    questions.forEach((element) => {
+      var row = table.insertRow(); // Insertion d'une nouvelle ligne dans le tableau
+
+      var questionCell = row.insertCell(); // Insertion de la cellule pour la question
+      questionCell.innerHTML =
+        'Q.' +
+        [questions.indexOf(element) + 1] +
+        '/' +
+        nbQuestions +
+        ' Id.' +
+        element.index +
+        ' : ' +
+        element.texte;
+      questionCell.style.border = '1px solid white'; // Ajout de la bordure à la cellule
+
+      var correctionCell = row.insertCell(); // Insertion de la cellule pour la correction
+      correctionCell.innerHTML =
+        propositions[questions.indexOf(element)][
+          corrections[questions.indexOf(element)].indexOf(true)
+        ];
+      correctionCell.style.border = '1px solid white'; // Ajout de la bordure à la cellule
+
+      var reponseCell = row.insertCell(); // Insertion de la cellule pour la reponse donné par l'utilisateur
+      reponseCell.innerHTML = tabValeurSelectionner[questions.indexOf(element)];
+      reponseCell.style.border = '1px solid white'; // Ajout de la bordure à la cellule
+      if (reponseCell.innerHTML == 'undefined') {
+        reponseCell.innerHTML = 'Question sauté / Réponse invalide';
+      }
+    });
+    exerciceContainer.appendChild(table); // Ajout du tableau au conteneur
+
+    MathJax.typesetPromise([exerciceContainer]).catch((err) =>
+      console.error(err)
+    );
+  }
+
+  let p = 0; // Pourcentage de pénalité initial
+
+  if (penalite) {
+    const parsedP = parseInt(penalite);
+    if (!isNaN(parsedP) && parsedP >= 0 && parsedP <= 100) {
+      p = parsedP;
+    } else {
+      console.error(
+        'Erreur lors de la récupération du pourcentage de pénalité :',
+        penalite
+      );
+    }
+  }
+
+  function getNbQuestions() {
+    nbQuestions = listeQuestionsString.split(' ').length;
+  }
+
+  function getUserId() {
+    let userID = localStorage.getItem('userID');
+
+    if (!userID) {
+      // Générer un nouvel ID utilisateur unique
+      userID = generateUniqueID();
+
+      // Stocker l'ID utilisateur dans le localStorage
+      localStorage.setItem('userID', userID);
+    }
+
+    return userID;
+  }
+
+  function getListeQuestions() {
+    if (listeQuestionsString == null) {
+      document.querySelector('.accueil').innerHTML =
+        "<h1>Bienvenue !</h1> <br> <h3>Pour commencer, veuillez saisir une liste de question dans les paramètres de l'URL. <br> Voici par exemple un quiz contenant les question : 1,2,3,4 et 5 avec l'ordre d'affichage des questions randomnisées et sans pénalité : <br> <a href='https://phil1010.github.io/quizapp-exo7/?c=1+2+3+4+5&r=1&p=0'>https://phil1010.github.io/quizapp-exo7/?c=1+2+3+4+5&r=1&p=0</a> </h3> ";
+      document.querySelector('#btnNext').style.display = 'none';
+      return;
+    }
+    listeQuestions.push(...listeQuestionsString.split(' '));
+    listeQuestions = listeQuestions.map((question) => parseInt(question));
+  }
+
+  function getNoteSur100() {
+    noteSur100 = (scoreSur20 * 100) / 20;
   }
 </script>
 
 <main>
-  <div class="question">
+  <div class="accueil">
+    {#if listeQuestionsString != null}
+      <h1>Bienvenue !</h1>
+      {#if nickName == null}
+        Entrez votre pseudo (facultatif)
+        <br />
+        <br />
+        <input
+          id="pseudo"
+          type="text"
+          style="border-radius: 5%;"
+          maxlength="15"
+        />
+        <br />
+      {:else if nickName != ''}
+        <h3>Bonjour {nickName}</h3>
+      {/if}
+      <h3>
+        Le quiz contient {nbQuestions} questions. <br />
+        Si vous etes prêt(e) appuyez sur le bouton commencer !
+      </h3>
+      <br />
+
+      <button id="btnStart" on:click={demarrer}> Commencer </button>
+    {/if}
+  </div>
+
+  <div class="question" style="display: none;">
     {#if questions[questionIndex]}
       <Question
         question={questions[questionIndex]}
         propositions={propositions[questionIndex]}
         onSelectedValue={handleSelectedValue}
+        nbQuestions={questions.length}
       />
     {/if}
   </div>
-  <button id="btnNext" on:click={nextQuestion}> Next </button>
-  <div class="score" style="display: none;">Score : {score}</div>
+  <button id="btnNext" on:click={nextQuestion} style="display: none;">
+    Next
+  </button>
+  <div class="score" style="display: none;">Score : {scoreSur20} / 20</div>
+  <div class="timer" style="display: none;">
+    Temps: {temps}s
+  </div>
+  <div class="detailsQuestions" style="display: none;">
+    Nombre de bonne réponse : {nbQuestionReussi} <br /> Nombre de mauvaise
+    réponse : {nbQuestionEchoue} <br /> Nombre de question sauté : {nbQuestionSaute}
+  </div>
+  <br />
+  <div id="ligneEnonceCompile" style="margin-bottom: 5rem;" />
+
+  <br />
+  <button id="voirCorrection" on:click={cliqueCorrection} style="display: none;"
+    >Voir le corrigé</button
+  >
+  <br />
+  <button
+    id="cacherCorrection"
+    on:click={() => {
+      document.querySelector('#voirCorrection').style.display = '';
+      document.querySelector('#cacherCorrection').style.display = 'none';
+      document.querySelector('#ligneEnonceCompile').innerHTML = '';
+    }}
+    style="display: none;">Caché le corrigé</button
+  >
+  <br />
+
+  <br />
+  <button id="btnRejouer" on:click={rejouer} style="display: none;">
+    Rejouer
+  </button>
+
+  <div />
 </main>
 
 <style>
-  /* Ajoutez ici vos styles CSS personnalisés */
+  /* stacker les boutons en bas centrer */
+  button {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 10rem;
+    height: 50px;
+    border: none;
+    color: white;
+    text-align: center;
+    /* centrer */
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    font-size: 16px;
+    margin-bottom: 2rem;
+  }
+  /* bouton voir correction au dessus du bouton rejouer */
+  #voirCorrection {
+    position: fixed;
+    bottom: 50px;
+    left: 0;
+    width: 10rem;
+    height: 50px;
+    border: none;
+    color: white;
+    text-align: center;
+    /* centrer */
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    font-size: 16px;
+    margin-bottom: 4rem;
+  }
+  #cacherCorrection {
+    position: fixed;
+    bottom: 50px;
+    left: 0;
+    width: 10rem;
+    height: 50px;
+    border: none;
+    color: white;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    font-size: 16px;
+    margin-bottom: 4rem;
+  }
+
+  /* augmenter la taille de l'input du pseudo */
+  input {
+    width: 15rem;
+    height: 2rem;
+  }
 </style>
--- my-app/src/Question.svelte
+++ my-app/src/Question.svelte
@@ -1,16 +1,28 @@
 <script>
-  import { afterUpdate, onMount } from 'svelte';
+  import { afterUpdate, beforeUpdate, onMount } from 'svelte';
 
   export let question;
   export let propositions;
   export let onSelectedValue;
+  export let nbQuestions;
+  export let cpt = 0;
 
-  function handleRadioButtonClick(event) {
-    const selectedIndex = event.target.value;
+  function getSelectedIndex() {
+    const selectedIndex = [];
+    document.querySelectorAll('input[type=checkbox]').forEach((el) => {
+      if (el.checked) {
+        selectedIndex.push(el.value);
+      }
+    });
+    return selectedIndex;
+  }
+
+  function handleCheckBox() {
+    const selectedIndex = getSelectedIndex();
     onSelectedValue(selectedIndex);
   }
 
-  function compileFormuleMathematique() {
+  export function compileFormuleMathematique() {
     var exerciceContainer = document.getElementById('questionTexte');
     exerciceContainer.innerHTML = question.texte;
 
@@ -18,18 +30,22 @@
       console.error(err)
     );
   }
-  onMount(() => {
-    compileFormuleMathematique();
-  });
 
   afterUpdate(() => {
     compileFormuleMathematique();
+    // décocher les checkbox
+    document.querySelectorAll('input[type=checkbox]').forEach((el) => {
+      el.checked = false;
+    });
+    // pour chaque question qui s'affiche leur donné un index et les incrémente
+    cpt++;
   });
 </script>
 
 <main>
   <div class="question">
-    <h1>Question {question.index}</h1>
+    <h1>Question {cpt}/{nbQuestions}</h1>
+    <h3>id : {question.index}</h3>
     <div class="enonce">
       <div id="questionTexte" />
       <br />
@@ -37,11 +53,11 @@
         {#each propositions as proposition, index}
           <div class="proposition">
             <input
-              type="radio"
+              type="checkbox"
               id="proposition"
               name="proposition"
               value={index}
-              on:click={handleRadioButtonClick}
+              on:click={handleCheckBox}
             />
             <label for={index}>{proposition}</label>
           </div>
@@ -53,4 +69,7 @@
 </main>
 
 <style>
+  label {
+    font-size: 1.5em;
+  }
 </style>
